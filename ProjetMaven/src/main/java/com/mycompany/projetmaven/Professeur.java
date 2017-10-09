0db3ba32c91fdbe55a1c4b2e7707b195d3194ab8
ahmad.guisse <ahmad.guisse@gmail.com>
2017-10-09 22:59:32 +0000

    ajout de la classe personne

---
+++ ProjetMaven/src/main/java/com/mycompany/projetmaven/Professeur.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.projetmaven;
+
+/**
+ *
+ * @author guisse
+ */
+public class Professeur extends Personne{
+
+    public Professeur() {
+    }
+
+    public Professeur(String nom, String prenom, int age) {
+        super(nom, prenom, age);
+    }
+}
